dff8ad11085270a374719f476dc1c051a9a809e9
joyeajin <dpwls31200@gmail.com>
2022-07-11 17:47:35 +0900

    first commit

---
+++ js1.js
@@ -0,0 +1,17 @@
+let value1=2022+'2023';
+console.log(typeof value1);
+
+let str1 = 2023;
+let str2 = '안녕'+str1+'년'
+let str3 = `안녕하세요 올해는
+ ${str1}년입니다.`
+console.log(str3)
+
+
+let isOn=true;
+let isOff=false;
+console.log(isOn)
+console.log(typeof isOff)
+
+let js1;
+console.log(typeof js1)
---
+++ js2.js
@@ -0,0 +1,11 @@
+
+//이것들을 한번에 쓰려면!
+
+let user = {
+    name:'joyeajin',
+    age:29,
+    address:'seoul',
+    tel:'01067577180',
+}
+
+console.log(user)
